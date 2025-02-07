bcd808508b925d8a5c18107684c31e63a662648e
SofiaSanchezP <129930051+SofiaSanchezP@users.noreply.github.com>
2025-02-07 02:14:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+const headerElement = document.querySelector("header");
+const mainElement = document.querySelector("main");
+const sectionElement = document.querySelector("section");
+
+const btnSacar = document.querySelector("#sacar");
+const btnQuemar = document.querySelector("#quemar");
+const btnVolver = document.querySelector("#volver"); // Volver al header
+const btnOpenElement = document.querySelector("#open");
+const btnCloseElement = document.querySelector("#close");
+const btnReiniciar = document.querySelector("#reiniciar");
+const paperElement = document.querySelector(".paper");
+const coverElement = document.querySelector(".cover");
+
+// Ocultar el main al inicio
+mainElement.style.display = "none";
+sectionElement.style.display = "none";
+
+// Mostrar el main y ocultar el header al presionar "Sacar la carta"
+btnSacar.addEventListener("click", () => {
+  headerElement.style.display = "none";
+  mainElement.style.display = "flex"; // Se muestra el main
+});
+
+// La carta inicia cerrada
+btnCloseElement.disabled = true;
+paperElement.classList.add("close-paper");
+
+// Evento para abrir la carta
+btnOpenElement.addEventListener("click", () => {
+  btnOpenElement.disabled = true;
+  btnCloseElement.disabled = false;
+
+  coverElement.classList.add("open-cover");
+
+  setTimeout(() => {
+    coverElement.style.zIndex = -1;
+    paperElement.classList.remove("close-paper");
+    paperElement.classList.add("open-paper");
+  }, 500);
+});
+
+// Evento para cerrar la carta y animar el sobre
+btnCloseElement.addEventListener("click", () => {
+  btnOpenElement.disabled = false; // Permite volver a abrirla
+  btnCloseElement.disabled = true; // Deshabilita el botón de cerrar
+
+  // Cerrar el papel con animación
+  paperElement.classList.remove("open-paper");
+  paperElement.classList.add("close-paper");
+
+  // Restaurar la tapa con animación
+  setTimeout(() => {
+    coverElement.style.zIndex = 1; // Trae la tapa al frente
+    coverElement.classList.remove("open-cover"); // Cierra la tapa
+  }, 500);
+});
+
+// Evento para volver al header SIN cerrar la carta
+btnVolver.addEventListener("click", () => {
+  mainElement.style.display = "none";
+  headerElement.style.display = "flex";
+});
+
+// Evento para perder
+btnQuemar.addEventListener("click", () => {
+  headerElement.style.display = "none";
+  sectionElement.style.display = "flex";
+});
+
+// Evento para reiniciar
+btnReiniciar.addEventListener("click", () => {
+  sectionElement.style.display = "none";
+  headerElement.style.display = "flex";
+});
