d6feee5a4ea34dac965199721fafa45eb8288a0b
SofiaSanchezP <129930051+SofiaSanchezP@users.noreply.github.com>
2025-02-10 17:56:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
   const elements = {
     header: document.querySelector("header"),
     main: document.querySelector("main"),
